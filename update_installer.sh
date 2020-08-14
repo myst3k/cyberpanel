df287eb7ce2aaa534bd246dbb6669bb337f1d1a4
chris <chris@ph4te.net>
2020-08-14 13:28:13 -0400

    updating update script

--- update_installer.sh
+++ update_installer.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
-wget --no-cache -q -O cyberpanel.sh https://raw.githubusercontent.com/myst3k/cyberpanel/custom/cyberpanel.sh?flush_cache=True
+GIT_URL="github.com/myst3k/cyberpanel"
+GIT_BRANCH="custom"
+
+GIT_DIR=$(dirname "$GIT_URL")
+GIT_USER=$(basename "$GIT_DIR")
+GIT_REPO=$(basename "$GIT_URL")
+GIT_OPTS="?flush_cache=True"
+GIT_CONTENT_URL="raw.githubusercontent.com/$GIT_USER/$GIT_REPO/$GIT_BRANCH"
+
+wget --no-cache -q -O cyberpanel.sh "$GIT_CONTENT_URL"/cyberpanel.sh"$GIT_OPTS"
 chmod 755 cyberpanel.sh
+
+echo "Run Command: GIT_URL=$GIT_URL BRANCH_NAME=$GIT_BRANCH ./cyberpanel.sh"
